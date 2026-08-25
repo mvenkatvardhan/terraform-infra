e5cb68b8238edaaf347690616c0e2e0b3caa7517
mvenkatvardhan <mvenkatvardhan05@gmail.com>
2026-08-25 12:58:24 +0530

    Replace internship key pair with deployer key pair

--- main.tf
+++ main.tf
@@ -113,19 +113,20 @@ resource "aws_security_group" "intern_sg" {
 # =========================================================
 # Key Pair (Terraform generates a new one)
 # =========================================================
-resource "tls_private_key" "this" {
+resource "tls_private_key" "deployer" {
   algorithm = "RSA"
   rsa_bits  = 4096
 }
 
-resource "aws_key_pair" "internship" {
-  key_name   = "internship"
-  public_key = tls_private_key.this.public_key_openssh
+resource "aws_key_pair" "deployer" {
+  key_name   = "gitlab-deploy-key"
+  public_key = tls_private_key.deployer.public_key_openssh
 }
 
-resource "local_file" "private_key" {
-  content         = tls_private_key.this.private_key_pem
-  filename        = "${path.module}/internship.pem"
+# This writes the .pem file to your terraform folder when you run `terraform apply`
+resource "local_sensitive_file" "private_key" {
+  content         = tls_private_key.deployer.private_key_pem
+  filename        = "${path.module}/gitlab-deploy-key.pem"
   file_permission = "0400"
 }
 
