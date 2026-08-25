ab39519f373e0547744a4e3688e200a3f50282bf
mvenkatvardhan <mvenkatvardhan05@gmail.com>
2026-08-25 12:01:27 +0530

    Modify egress rules in main.tf
    Removed specific egress rules for SSH and HTTP, replaced with a catch-all egress rule.

--- main.tf
+++ main.tf
@@ -99,20 +99,11 @@ resource "aws_security_group" "intern_sg" {
   }
 
   egress {
-    description = "SSH out"
-    from_port   = 22
-    to_port     = 22
-    protocol    = "tcp"
-    cidr_blocks = ["0.0.0.0/0"]
-  }
-
-  egress {
-    description = "HTTP out"
-    from_port   = 80
-    to_port     = 80
-    protocol    = "tcp"
-    cidr_blocks = ["0.0.0.0/0"]
-  }
+  from_port   = 0
+  to_port     = 0
+  protocol    = "-1"
+  cidr_blocks = ["0.0.0.0/0"]
+}
 
   tags = {
     Name = "intern-sg"
